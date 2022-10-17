de9fcafaee23fe16d034e35196ffdde803fc2c64
Alex <flatalex123@gmail.com>
2022-10-17 20:21:41 +1000

    Rooms on home page can now input as many rooms as needed

--- my-app/src/Pages/HomePage.js
+++ my-app/src/Pages/HomePage.js
@@ -3,45 +3,40 @@ import { Link } from 'react-router-dom';
 import Slider from '../Components/graphSlider/Slider';
 import classes from '../css/homePage.module.css'
 import '../css/test.css'
-
+import RoomTableItem from '../Components/RoomsTableItem'
 export default function HomePage() {
-
+    const [roomDetails, setRoomDetails] = useState( [{ Name: "default", inUse: false }])
     return (
         <div className={classes.layout}>
             <div className="grid-container">
                 <div className={classes.header}>
-                    <b>Hospital DashBoard</b>
+                    <b>Hospital Dashboard</b>
                 </div>
                 <div className={classes.card} >
                     <div style={{ lineHeight: 2 }}>
                         <b>Alerts</b>
-                        <p>Room usage</p>
-                        <p>Machine usage</p>
+                        <div style={{ display: "grid", gridTemplateRows: "100% 100%", marginBottom: "30px", height: "30px" }}>
+                            <text>Room usage:</text>
+                            <text>Machine usage:</text>
+                        </div>
                     </div>
                 </div>
                 <div className={classes.card}>
                     <b>Statistics</b>
-                    <div style={{ overflow: 'hidden' }} >
+                    <div style={{ height: "230px", overflow: 'hidden' }} >
                         <Slider ></Slider>
                     </div>
 
                 </div>
 
-                <div className={classes.card} style={{ height: "185px", overflowY: "scroll" }}>                        
-                <b>Rooms</b>
-
+                <div className={classes.card} style={{ height: "253px", overflowY: "scroll" }}>
+                    <b>Rooms</b>
                     <div className={classes.roomCard}>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
-                        <p>asdfa</p>
+                        {roomDetails.map((e) => (
+                            <RoomTableItem roomName={e.Name} inUse={e.inUse}></RoomTableItem>
+                        ))}
+
                     </div>
-
-
                 </div>
                 <div className={classes.navBar}>
                     <Link to={"/Map"}>
